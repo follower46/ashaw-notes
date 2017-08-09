42dddd5fcf65b0c8b95094e56d3d7f6183324810
Adam Shaw <adamryanshaw@gmail.com>
2017-08-08 23:01:43 -0500

    Updated with more tests

--- tests/test_connectors_redis_notes.py
+++ tests/test_connectors_redis_notes.py
@@ -33,3 +33,18 @@ class LocalNotesTests(unittest.TestCase):
         """Verifies save_note is properly functioning"""
         redis_notes.save_note(12345, "test note")
         add_redis_note.assert_called_once_with(12345, "test note")
+    
+    @patch('connectors.redis_notes.delete_redis_note')
+    def test_delete_note(self, delete_redis_note):
+        """Verifies delete_note is properly functioning"""
+        redis_notes.delete_note(12345)
+        delete_redis_note.assert_called_once_with(12345)
+
+
+    @patch('connectors.redis_notes.save_note')
+    @patch('connectors.redis_notes.delete_redis_note')
+    def test_update_note(self, delete_redis_note, save_note):
+        """Verifies update_note is properly functioning"""
+        redis_notes.update_note(12345, 23456, "test note")
+        delete_redis_note.assert_called_once_with(12345)
+        save_note.assert_called_once_with(23456, "test note")
