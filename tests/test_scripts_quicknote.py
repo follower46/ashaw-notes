f2225543e4a62281567b1bc90985dffbf5385b64
Adam Shaw <adamryanshaw@gmail.com>
2017-08-11 11:36:51 -0500

    Changed unit tests to unblock build

--- tests/test_scripts_quicknote.py
+++ tests/test_scripts_quicknote.py
@@ -10,6 +10,6 @@ from ddt import ddt, data, unpack
 class QuicknoteTests(unittest.TestCase):
     """Unit Testing Quicknote"""
 
-    def test_setup_auto_complete(self, string, expectation):
+    def test_process_note(self):
         """Verifies is_date is properly functioning"""
-        quicknote.setup_auto_complete()
+        quicknote.process_note('test')
